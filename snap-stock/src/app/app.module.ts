39273e1c6ab18dbfb7f33cecaf4da5e38a64c2cb
gregSWEN <69919565+gregSWEN@users.noreply.github.com>
2023-02-25 20:40:20 -0500

    Skeleton

--- snap-stock/src/app/app.module.ts
+++ snap-stock/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HeaderComponent } from './header/header.component'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CameraComponentComponent } from './camera-component/camera-component.component';
-
+import { FooterComponent } from './footer/footer.component';
+import { CameraComponent } from './camera/camera.component';
+import { ManualInputComponent } from './manual-input/manual-input.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CameraComponentComponent,
-    CameraComponentComponent
+    HeaderComponent,
+    FooterComponent,
+    CameraComponent,
+    ManualInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ snap-stock/src/app/manual-input/manual-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-input',
+  templateUrl: './manual-input.component.html',
+  styleUrls: ['./manual-input.component.css']
+})
+export class ManualInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
