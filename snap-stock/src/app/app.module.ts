715b2e479f0489a9c62b19a77de7ed37d2651807
gregSWEN <69919565+gregSWEN@users.noreply.github.com>
2023-02-25 20:17:53 -0500

    reverting

--- snap-stock/src/app/app.module.ts
+++ snap-stock/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import {WebcamModule} from 'ngx-webcam';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponentComponent } from './header-component/header-component.component';
-import { FooterComponentComponent } from './footer-component/footer-component.component';
 import { CameraComponentComponent } from './camera-component/camera-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponentComponent,
-    FooterComponentComponent,
     CameraComponentComponent,
   ],
   imports: [
