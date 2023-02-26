ea76e1d3efaf3a9cf44ee09ad2304226bfb69b69
Kevin Gomez <kevingomez@kevins-mbp-8.wireless.rit.edu>
2023-02-25 21:42:17 -0500

    Extension of api implementation

--- snap-stock/src/app/app.component.ts
+++ snap-stock/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AppService } from './app.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'snap-stock';
+export class AppComponent implements OnDestroy {
+  constructor(private appService: AppService) {}
+
+  title = 'angular-nodejs-example';
+
+  userForm = new FormGroup({
+    firstName: new FormControl('', Validators.nullValidator && Validators.required),
+    lastName: new FormControl('', Validators.nullValidator && Validators.required),
+    foodInventory: new FormControl('', Validators.nullValidator && Validators.required)
+  });
+
+  user: any;
+  userCount = 0;
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  onSubmit(): void {
+    this.appService.addUser(this.userForm.value).pipe(takeUntil(this.destroy$)).subscribe(data => {
+      console.log('message::::', data);
+      this.userCount = this.userCount + 1;
+      console.log(this.userCount);
+      this.userForm.reset();
+    });
+  }
+
+  getAllUsers(): void {
+    this.appService.getUser().pipe(takeUntil(this.destroy$)).subscribe((user: any) => {
+      this.user = user;
+    });
+  }
+
+  ngOnDestroy(): void{
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
