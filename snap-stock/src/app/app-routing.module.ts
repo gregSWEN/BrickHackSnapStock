c61b557d352f66668c08300a16cbff3becae0789
gregSWEN <69919565+gregSWEN@users.noreply.github.com>
2023-02-25 20:03:14 -0500

    Initial commit

--- snap-stock/src/app/app-routing.module.ts
+++ snap-stock/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CameraComponentComponent} from './camera-component/camera-component.component';
+import {AppComponent} from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: 'main', component: AppComponent},
+  {path: 'camera', component: CameraComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- snap-stock/src/app/app.module.ts
+++ snap-stock/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { CameraComponentComponent } from './camera-component/camera-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    CameraComponentComponent
   ],
   imports: [
     BrowserModule,
